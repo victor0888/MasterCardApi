e803a0eff721c7e574ce6bcb93282c390cebc1cb
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-22 18:19:27 +1000

    Update redirect_form.php

--- redirect_form.php
+++ redirect_form.php
@@ -3,25 +3,25 @@
 <head>
 <title>Redirect Form To a Particular Page On Submit - Demo Preview</title>
 <meta content="noindex, nofollow" name="robots">
-<link href='css/redirect_form.css' rel='stylesheet' type='text/css'> <!--== Include CSS File Here ==-->
+<link href='redirect_form.css' rel='stylesheet' type='text/css'> <!--== Include CSS File Here ==-->
 </head>
 <body>
 <div class="main">
 <div class="first">
 <h2>Redirect Form To a Particular Page On Submit using PHP</h2>
 <form action="redirect_form.php" id="#form" method="post" name="#form">
-<label>Name :</label>
-<input id="name" name="name" placeholder='Your Name' type='text'>
-<label>Email :</label>
-<input id="email" name="email" placeholder='Valid Email Address' type='text'>
-<label>Contact :</label>
-<input id="contact" name="contact" placeholder='Contact' type='text'>
-<label>Address:</label>
-<input id="address" name="address" placeholder='Address' type='text' value="">
+<label>API Username :</label>
+<input id="username" name="username" placeholder='Your API Username' type='text'>
+<label>API Password :</label>
+<input id="password" name="password" placeholder='Valid API Password' type='text'>
+<label>Currency :</label>
+<input id="currency" name="currency" placeholder='AUD' type='text'>
+<label>OrderId:</label>
+<input id="orderid" name="orderid" placeholder='Order ID' type='text' value="">
 <input id='btn' name="submit" type='submit' value='Submit'>
 <!---- Including PHP File Here ---->
 <?php
-include "redirect.php";
+include "CreateCheckoutSessionExample.php";
 ?>
 </form>
 </div>
