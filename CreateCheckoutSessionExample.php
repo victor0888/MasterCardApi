23cf193407e744cae979299f8f1537efd28b6217
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-22 18:26:32 +1000

    Update CreateCheckoutSessionExample.php

--- CreateCheckoutSessionExample.php
+++ CreateCheckoutSessionExample.php
@@ -4,22 +4,21 @@
 $curl = curl_init();
 if(isset($_POST['submit'])){
 // Fetching variables of the form which travels in URL
-$name = $_POST['name'];
-$email = $_POST['email'];
-$contact = $_POST['contact'];
-$address = $_POST['address'];
-if($name !=''&& $email !=''&& $contact !=''&& $address !='')
+$username = "merchant".$_POST['username'];
+$merchant = $_POST['username'];
+$password = $_POST['password'];
+$currency = $_POST['currency'];
+$orderid = $_POST['orderid'];
+if($username !=''&& $password !=''&& $currency !=''&& $orderid !='')
 {
 //  To redirect form on a particular page
 header("Location:https://victor-test-app123.herokuapp.com/CreateCheckoutSessionExample.php");
 }
 else{
-?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
-}
-}
-?>
-$username = "merchant.TESTVICTESNB237";
-$password = "ed17a6e2ed43172c0f7bde912cbe54de";
+<span> echo "Please fill all fields.....!!!!!!!!!!!!";</span>
+    }
+$username1 = "merchant.TESTVICTESNB237";
+$password1 = "ed17a6e2ed43172c0f7bde912cbe54de";
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://gateway.test.nab.com.au/api/nvp/version/49",
   CURLOPT_RETURNTRANSFER => true,
@@ -28,7 +27,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=TESTVICTESNB237&apiUsername=merchant.TESTVICTESNB237&apiPassword=ed17a6e2ed43172c0f7bde912cbe54de&order.currency=AUD&order.id=546098tr8834&order.reference=45234224rt8tii&transaction.reference=3452345p6fg5ff&order.amount=5&interaction.returnUrl=https%3A%2F%2Ftigris-eden.com%2F&lineOfBusiness=test_socks",
+  CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$username&apiPassword=$password&order.currency=AUD&order.id=546098tr8834&order.reference=4523422frt8tii&transaction.reference=3452335ff&order.amount=5&interaction.returnUrl=https%3A%2F%2Ftigris-eden.com%2F&lineOfBusiness=test_socks",
   CURLOPT_HTTPHEADER => array(
 'Authorization: Basic ' . base64_encode("$username:$password"))
 ));
@@ -46,7 +45,7 @@ echo "<br>".$merchant."<br>";
 echo $result."<br>";
 echo $session_id."<br>";
 echo $session_updateStatus."<br>";
-echo $session_updateStatus."<br>";
-echo $session_version;
+echo $session_version"<br>";
+echo $success_Indicator;
 ?>
 }
