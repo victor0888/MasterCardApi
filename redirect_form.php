bbb4858e1ce47205c33373c57ac16a8f3cf54645
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-23 15:37:52 +1000

    Update redirect_form.php

--- redirect_form.php
+++ redirect_form.php
@@ -11,13 +11,13 @@
 <h2>Redirect Form To a Particular Page On Submit using PHP</h2>
 <form action="redirect.php" id="#form" method="post" name="#form">
 <label>API Username :</label>
-<input id="username" name="username" placeholder='Your API Username' type='text'>
+<input id="username" name="username" value="<?php echo $username?>" placeholder='Your API Username' type='text'>
 <label>API Password :</label>
-<input id="password" name="password" placeholder='Valid API Password' type='text'>
+<input id="password" name="password" value="<?php echo $password?>" placeholder='Valid API Password' type='text'>
 <label>Currency :</label>
-<input id="currency" name="currency" placeholder='AUD' type='text'>
+<input id="currency" name="currency" value="<?php echo $currency?>" placeholder='AUD' type='text'>
 <label>OrderId:</label>
-<input id="orderid" name="orderid" placeholder='Order ID' type='text' value="">
+<input id="orderid" name="orderid" value="<?php echo $orderid?>" placeholder='Order ID' type='text' value="">
 <input id='btn' name="submit" type='submit' value='Submit'>
 <!---- Including PHP File Here ---->
 </form>
@@ -26,24 +26,4 @@
 </body>
 </html>
 
-<?php
-if(isset($_POST['submit'])){
-// Fetching variables of the form which travels in URL
-  
-$username = "merchant".$_POST['username'];
-$merchant = $_POST['username'];
-$password = $_POST['password'];
-$currency = $_POST['currency'];
-$orderid = $_POST['orderid'];
-if($username !=''&& $password !=''&& $currency !=''&& $orderid !='')
-{
-//  To redirect form on a particular page
-header("Location:https://victor-test-app123.herokuapp.com/redirect.php"); 
-}
-else{
-?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
-}
-  
-}
-?>
 <span><?php echo $username;?></span> 
