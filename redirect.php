55aac8f454c0b3d4374849541e6f2f1d9c94b409
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-23 15:31:57 +1000

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,22 +1,5 @@
 <?php
-if(isset($_POST['submit'])){
-// Fetching variables of the form which travels in URL
-  
-$username = "merchant".$_POST['username'];
-$merchant = $_POST['username'];
-$password = $_POST['password'];
-$currency = $_POST['currency'];
-$orderid = $_POST['orderid'];
-if($username !=''&& $password !=''&& $currency !=''&& $orderid !='')
-{
-//  To redirect form on a particular page
-header("Location:https://victor-test-app123.herokuapp.com/redirect.php"); 
-}
-else{
-?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
-}
-  
-}
+ <?php include "redirect_form.php";?>
 ?>
 <span><?php echo $username;?></span> 
 
