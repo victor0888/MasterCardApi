ee65fb13a8cf29e97311c5460e529763a63b9422
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-23 14:04:55 +1000

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -6,13 +6,12 @@ $merchant = $_POST['username'];
 $password = $_POST['password'];
 $currency = $_POST['currency'];
 $orderid = $_POST['orderid'];
+    echo $username;<br>
+   echo $password;<br>
 if($username !=''&& $password !=''&& $currency !=''&& $orderid !='')
 {
 //  To redirect form on a particular page
-//header("Location:https://victor-test-app123.herokuapp.com/redirect.php");
-  echo $username;<br>
-   echo $password;<br>
-  
+header("Location:https://victor-test-app123.herokuapp.com/redirect.php"); 
 }
 else{
 ?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
