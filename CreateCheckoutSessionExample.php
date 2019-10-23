9a943b52bd4b3d5a73fef8a875986767b2d3f6b0
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-23 14:33:31 +1000

    Update CreateCheckoutSessionExample.php

--- CreateCheckoutSessionExample.php
+++ CreateCheckoutSessionExample.php
@@ -1,9 +1,15 @@
 
 <?php
 include "redirect.php";
+echo $username;
+echo $merchant;
+echo $password;
+echo $currency;
+echo $orderid; 
+
 $curl = curl_init();
-$username1 = "merchant.TESTVICTESNB237";
-$password1 = "ed17a6e2ed43172c0f7bde912cbe54de";
+$username = "merchant.TESTVICTESNB237";
+$password = "ed17a6e2ed43172c0f7bde912cbe54de";
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://gateway.test.nab.com.au/api/nvp/version/49",
   CURLOPT_RETURNTRANSFER => true,
@@ -12,7 +18,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$username&apiPassword=$password&order.currency=AUD&order.id=546098tr8834&order.reference=4523422frt8tii&transaction.reference=3452335ff&order.amount=5&interaction.returnUrl=https%3A%2F%2Ftigris-eden.com%2F&lineOfBusiness=test_socks",
+  CURLOPT_POSTFIELDS => "apiOperation=CREATE_CHECKOUT_SESSION&merchant=$merchant&apiUsername=$username&apiPassword=$password&order.currency=$AUD&order.id=546098tr8834&order.reference=4523422frt8tii&transaction.reference=3452335ff&order.amount=5&interaction.returnUrl=https%3A%2F%2Ftigris-eden.com%2F&lineOfBusiness=test_socks",
   CURLOPT_HTTPHEADER => array(
 'Authorization: Basic ' . base64_encode("$username:$password"))
 ));
