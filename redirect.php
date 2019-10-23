5070a86336ad35a2d12ae222cfc0010eabe93951
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-23 13:40:39 +1000

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -10,6 +10,9 @@ if($username !=''&& $password !=''&& $currency !=''&& $orderid !='')
 {
 //  To redirect form on a particular page
 header("Location:https://victor-test-app123.herokuapp.com/redirect.php");
+  echo $username;<br>
+   echo $password;<br>
+  
 }
 else{
 ?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
