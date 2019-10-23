6927ead6372267defa595fcd3e474f0c7f0ad626
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-23 15:54:47 +1000

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,10 +1,19 @@
+ <?php 
+$username = "merchant".$_POST['username'];
+$merchant = $_POST['username'];
+$password = $_POST['password'];
+$currency = $_POST['currency'];
+$orderid = $_POST['orderid'];
 
+?>
 <html>
 
 <body>
 
-Hello <?php echo $_POST['username']; ?>
-
+Hello <?php echo $username; ?><br>
+Hello <?php echo $_POST['password']; ?><br>
+Hello <?php echo $_POST['password']; ?><br>
+Hello <?php echo $_POST['orderid']; ?><br>
 </body>
 
 </html>
