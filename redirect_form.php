bd74c7fadbe6560563622fc3e8673a3927500faa
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-23 18:17:32 +1000

    Update redirect_form.php

--- redirect_form.php
+++ redirect_form.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Redirect Form To a Particular Page On Submit - Demo Preview</title>
+<title>Redirect To Create Checkout Session Request On Submit - Demo</title>
 <meta content="noindex, nofollow" name="robots">
 <link href='redirect_form.css' rel='stylesheet' type='text/css'> <!--== Include CSS File Here ==-->
 </head>
@@ -11,10 +11,13 @@
 <h2>Redirect Form To a Particular Page On Submit using PHP</h2>
 <form action="redirect.php" id="#form" method="post" name="#form">
 <label>API Username :</label>
+  <label>TEST950029025</label>
 <input id="username" name="username" value="<?php echo $_POST['username']; ?>" placeholder='Your API Username' type='text'>
 <label>API Password :</label>
+  <label>5aa689a4ff28a8d62e12a20e77133134</label>
 <input id="password" name="password"  value="<?php echo $_POST['password']; ?>" placeholder='Valid API Password' type='text'>
 <label>Currency :</label>
+  <label>BND</label>
 <input id="currency" name="currency" value="<?php echo $_POST['currency']; ?>"placeholder='AUD' type='text'>
 <label>OrderId:</label>
 <input id="orderid" name="orderid" value="<?php echo $_POST['orderid']; ?>" placeholder='Order ID' type='text' value="">
