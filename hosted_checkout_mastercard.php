b8ca7c966dd96dd593c38a512b788e90068612ad
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-24 16:28:22 +1000

    Update hosted_checkout_mastercard.php

--- hosted_checkout_mastercard.php
+++ hosted_checkout_mastercard.php
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-include 'redirect.php';
 
 # I am file2.php
 session_start(); # read up on session.auto_start
