c3ebd8d6be0d47560b8f314eb9313609e277e126
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-30 15:58:09 +1000

    Update redirect_form.php

--- redirect_form.php
+++ redirect_form.php
@@ -10,14 +10,20 @@
 <div class="first">
 <h2>Redirect To Create Checkout Session Request On Submit</h2>
 <form action="redirect.php" id="#form" method="post" name="#form">
+<label>Gateway Region URL :</label>
+  <label>test-gateway.mastercard.com</label>
+<input id="url" name="url" value="<?php echo $_POST['url']; ?>" placeholder='test-gateway.mastercard.com' type='text'>
+  <label>Gateway API Version :</label>
+<label>50</label>
+<input id="version" name="version" value="<?php echo $_POST['version']; ?>" placeholder='50' type='text'>
 <label>API Username :</label>
-  <label>TEST950029025</label>
+  <label>TESTVICTOR01</label>
 <input id="username" name="username" value="<?php echo $_POST['username']; ?>" placeholder='Your API Username' type='text'>
 <label>API Password :</label>
-  <label>5aa689a4ff28a8d62e12a20e77133134</label>
+  <label>cef2d7619cf1535c6b5320ee277a88ed</label>
 <input id="password" name="password"  value="<?php echo $_POST['password']; ?>" placeholder='Valid API Password' type='text'>
 <label>Currency :</label>
-  <label>BND</label>
+  <label>AUD</label>
 <input id="currency" name="currency" value="<?php echo $_POST['currency']; ?>"placeholder='AUD' type='text'>
 <label>OrderId:</label>
 <input id="orderid" name="orderid" value="<?php echo $_POST['orderid']; ?>" placeholder='Order ID' type='text' value="">
