d80d3f766852afd115e280d053975765877e5311
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-10-31 19:17:45 +1000

    Update hosted_checkout_mastercard.php

--- hosted_checkout_mastercard.php
+++ hosted_checkout_mastercard.php
@@ -17,7 +17,7 @@ session_start(); # read up on session.auto_start
  */
 $urlh = $_SESSION["url"];
 $versionh = $_SESSION["version"];
-$fullurl = "https://test-gateway.mastercard.com/checkout/version/$versionh/checkout.js";
+$fullurl = $urlh."https://"$urlh."/checkout/version/".$versionh."/checkout.js";
 ?>
 <!DOTYPE html>
 <html> 
