6d6db927041c54bce248c2c47120b2ad116e7bfc
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-11-07 16:40:03 +1000

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ include '_bootstrap.php';
             <li><a href="./threeds2.html">3DS JavaScript API with Session</a></li>
             <li><a href="./3dsRedirectSimple.php">3DS WSAPI Simple responseUrl</a></li>
             <li><a href="./redirect_form.php">Create Checkout Session with Redirect Form and pass session id to Hosted Checkout</a></li>
-            <li><a href="./webhook.html">Webhooks</a></li>
+            <li><a href="./webhook.php">Webhooks</a></li>
         </ul>
     </body>
 </html>
