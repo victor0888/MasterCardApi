b1cbb5b32554d621c3788febac8758d86d773fcb
victor0888 <47068746+victor0888@users.noreply.github.com>
2020-01-16 10:41:33 +1000

    Update index.php

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ include '_bootstrap.php';
             <li><a href="./3dsRedirectSimple.php">3DS WSAPI Simple responseUrl</a></li>
             <li><a href="./redirect_form.php">Create Checkout Session with Redirect Form and pass session id to Hosted Checkout</a></li>
             <li><a href="./threeds2.php">EMV 3DS2 Javascript</a></li>
+            <li><a href="./SimplifyCreateToken.php">Simplify Create Token Test</a></li>
         </ul>
     </body>
 </html>
